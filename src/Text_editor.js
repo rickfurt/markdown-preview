e4eb0fee637b2a64d7f020479e5f8e2c43cacde7
rickfurt <ricardofurtado2609@gmail.com>
2019-03-04 22:37:16 +1100

    Completed Markdown Previewer Project

--- src/Text_editor.js
+++ src/Text_editor.js
@@ -8,7 +8,7 @@ export default class Text_editor extends React.Component{
       
       this.state = {
         input:'',
-        markdown:'# Heading\n=======\n\n ## Sub-heading\n-----------\n \n### Another deeper heading\n [link](/link) \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears'
+        markdown:'# Header \n \n\n ## Sub-header \n-----------\n  ``` console.log() ``` \n-----------\n \n ``` \n function Test(){\n multi-line code \n} \n ```  \n### Another header \n [**Ricardo Furtado** Github](https://github.com/rickfurt) \n\n > This is a Blockquote \n\n blockquote \n\n \nParagraphs are Here  \n and Here.\n\n Need 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**.\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears \n\n **We can input images** \n\n ![foo](https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 "title")'
       }
       this.handleChange = this.handleChange.bind(this)
     }
@@ -32,7 +32,7 @@ export default class Text_editor extends React.Component{
         <div>
           <div className="container main-container">
             <div className="editor-container">
-              <div className="editor-header bg-secondary text-white">Markdown Previewer Editor</div>
+              <div className="editor-header bg-secondary text-white text-center">Markdown Previewer Editor</div>
                 <textarea className="form-control form-control-lg" id="editor" type="text" value={markdown} placeholder={markdown} onChange={(event)=> this.updateMark(event.target.value)}  />
             </div>
             <div id="preview-container">
